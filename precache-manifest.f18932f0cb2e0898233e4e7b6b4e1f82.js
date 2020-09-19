535fb4dd4b41f041f733c803ebdba05985bf7e05
patrickt <torralbapatrick15@gmail.com>
2020-09-19 08:34:07 -0500

    Updates

---
+++ precache-manifest.f18932f0cb2e0898233e4e7b6b4e1f82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90823e729336994e635bd8ef42c46fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a22fbb17f55b8a723112",
+    "url": "/static/css/2.7a6c1eb7.chunk.css"
+  },
+  {
+    "revision": "426f3be1fabd11754fde",
+    "url": "/static/css/main.33f153cf.chunk.css"
+  },
+  {
+    "revision": "a22fbb17f55b8a723112",
+    "url": "/static/js/2.dd7bad1c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dd7bad1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426f3be1fabd11754fde",
+    "url": "/static/js/main.401c51e5.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "04702e978e051d505260dcbf24a324c1",
+    "url": "/static/media/bee.04702e97.svg"
+  },
+  {
+    "revision": "36f6146b22e540e5c520e4589151fdea",
+    "url": "/static/media/games.36f6146b.jpg"
+  },
+  {
+    "revision": "28ddc47c61ac4ebe2b9b94b43ea17530",
+    "url": "/static/media/sneaker.28ddc47c.jpg"
+  },
+  {
+    "revision": "06fcff263379988b687b907ea8b687a9",
+    "url": "/static/media/sushi.06fcff26.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bizbee-ui/precache-manifest.b580b422081f92ab1826e248d13feba3.js"
+  "/precache-manifest.f18932f0cb2e0898233e4e7b6b4e1f82.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bizbee-ui/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
