2dc72e6f26eb2c66ee85b4816c20ef0716866cac
patrickt <torralbapatrick15@gmail.com>
2020-09-19 08:10:06 -0500

    Move build folder

---
+++ build/precache-manifest.b580b422081f92ab1826e248d13feba3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3993a1e011bd0a2b0f1e868b1e3b490",
+    "url": "/bizbee-ui/index.html"
+  },
+  {
+    "revision": "a22fbb17f55b8a723112",
+    "url": "/bizbee-ui/static/css/2.7a6c1eb7.chunk.css"
+  },
+  {
+    "revision": "426f3be1fabd11754fde",
+    "url": "/bizbee-ui/static/css/main.33f153cf.chunk.css"
+  },
+  {
+    "revision": "a22fbb17f55b8a723112",
+    "url": "/bizbee-ui/static/js/2.dd7bad1c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bizbee-ui/static/js/2.dd7bad1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426f3be1fabd11754fde",
+    "url": "/bizbee-ui/static/js/main.401c51e5.chunk.js"
+  },
+  {
+    "revision": "da4b2401b8234b71d44f",
+    "url": "/bizbee-ui/static/js/runtime-main.71684954.js"
+  },
+  {
+    "revision": "04702e978e051d505260dcbf24a324c1",
+    "url": "/bizbee-ui/static/media/bee.04702e97.svg"
+  },
+  {
+    "revision": "36f6146b22e540e5c520e4589151fdea",
+    "url": "/bizbee-ui/static/media/games.36f6146b.jpg"
+  },
+  {
+    "revision": "28ddc47c61ac4ebe2b9b94b43ea17530",
+    "url": "/bizbee-ui/static/media/sneaker.28ddc47c.jpg"
+  },
+  {
+    "revision": "06fcff263379988b687b907ea8b687a9",
+    "url": "/bizbee-ui/static/media/sushi.06fcff26.jpg"
+  }
+]);
